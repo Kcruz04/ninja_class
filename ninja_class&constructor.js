336fab7e814ddaf2040ba3d7cc7d1b66cc73e77e
Kcruz04 <kencruz515@gmail.com>
2023-05-02 09:15:21 -1000

    super ninja assignment

--- ninja_class&constructor.js
+++ ninja_class&constructor.js
@@ -9,20 +9,32 @@ class Ninja {
         console.log(`${this.name}`);
     }
     showStats() {
-        console.log(`${this.constructor.name}, ${this.constructor.health}, ${this.speed}, ${this.strength}`);
+        console.log(`${this.name}, ${this.health}, ${this.speed}, ${this.strength}`);
     }
     drinkSake() {
         this.health += 10;
     }
 }
+class Sensei extends Ninja {
+    constructor(name, health=200, wisdom=10) {
+        super(name, health)
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = wisdom;
+    }
+    speakWisdom(){
+        this.drinkSake()
+        console.log("What one programmer can do in one month, two programmers cann do in two months.")
+    }
 
-
-
-
+}
 
 
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
 
